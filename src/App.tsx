03f2c8f43787865b0e6a0de096833295d38c5e3d
Misode <Misoloo64@gmail.com>
2021-05-31 21:01:33 +0200

    Show error message when report fails to load

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ export function App() {
 	const [reports, setReports] = useState<Report[]>([])
 	const [activeTab, setTab] = useState(0)
 	const [activePanel, setPanel] = useState('Overview')
+	const [errors, setErrors] = useState<string[]>([])
 	const activeReport = reports[Math.min(reports.length-1, activeTab)]
 
 	const removeReport = (index: number) => {
@@ -32,9 +33,17 @@ export function App() {
 			}
 		}
 		if (promises.length > 0) {
-			const newReports = await Promise.all(promises)
+			const newReports = await Promise.all(promises.map(async promise => {
+				try {
+					return await promise
+				} catch (error) {
+					setErrors([...errors, error.message])
+					console.error(error)
+					return
+				}
+			}))
 			setTab(reports.length + newReports.length - 1)
-			setReports([...reports, ...newReports])
+			setReports([...reports, ...newReports.filter((r): r is Report => r !== undefined)])
 		} else {
 			const index = reports.findIndex(r => r.name === existingName)
 			if (index !== -1) setTab(index)
@@ -78,5 +87,16 @@ export function App() {
 				<p>Source code on <a href="https://github.com/misode/report" target="_blank">GitHub</a></p>
 			</div>
 		</>}
+		{errors.map(error => <div class="error">
+			<p>Something went wrong loading the report:</p>
+			<p class="error-message">{error}</p>
+			<p>What you can do:
+				<ul>
+					<li>Ensure that you are not using a version below 1.17-pre1</li>
+					<li>Report this as a bug <a href="https://github.com/misode/report/issues/new">on GitHub</a> and upload the report</li>
+				</ul>
+			</p>
+			<div class="error-close" onClick={() => setErrors(errors.filter(e => e !== error))}>x</div>
+		</div>)}
 	</main>
 }
